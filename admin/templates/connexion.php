167b2153bdb5f112eba26b01400521b447a4a1c6
iamNayth <contact@naythwork.fr>
2023-02-17 09:28:06 +0100

    maj

--- admin/templates/connexion.php
+++
@@ -1,8 +0,0 @@
-<?php $title = "Connexion"; ?>
-<?php ob_start(); ?>
-<section class="d-flex align-items-center justify-content-center">
-    <h1>Connexion</h1>
-
-</section>
-<?php $content = ob_get_clean();?>
-<?php require('layout.php') ?>
